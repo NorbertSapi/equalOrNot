306883aec59341886ffbe71c89e7d7ee14259fce
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-12 23:35:53 +0000

    final

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 # This is an exercise to determine if the sum of two integers is equal to the given value.
 
-numbers = [ 5, 7, 1, 2, 8, 4, 3]
-
+# numbers = [5, 7, 1, 2, 8, 4, 3]
 # for example
 # Target Sum: 10 = 7 + 3; 2 + 8
 # Target Sum: 19 = No two values sum up to 19
+
+
+def find_sum_of_two(x, target_number):
+    # iterate through list, first number
+    for a in x:
+        # iterate through list, second number
+        for b in x:
+            if b == a:
+                continue
+            if b == (target_number-a):
+                print(target_number, " - ", a, " = ", b)
+                return True
+    return False
+
+
+print(find_sum_of_two([5, 7, 1, 2, 8, 4, 3], 12))
+print(find_sum_of_two([5, 7, 1, 2, 8, 4, 3], 15))
+print(find_sum_of_two([5, 2, 8, 7, 6, 1, 9], 16))
+print(find_sum_of_two([5, 2, 8, 7, 6, 1, 9], 19))
