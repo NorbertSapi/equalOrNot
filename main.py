acdb6e470b284699532e1fdd6d1a94835fe4538e
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-12 14:30:48 +0000

    Initial commit Determine if there are any two numbers from the list whose sum is equal to the given number. 'true' if sum exist 'false' if sum does not exist

---
+++ main.py
@@ -0,0 +1,7 @@
+# This is an exercise to determine if the sum of two integers is equal to the given value.
+
+numbers = [ 5, 7, 1, 2, 8, 4, 3]
+
+# for example
+# Target Sum: 10 = 7 + 3; 2 + 8
+# Target Sum: 19 = No two values sum up to 19
